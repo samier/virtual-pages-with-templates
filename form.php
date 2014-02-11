dc9ca4db39204ee9b64fd5ccf18f5cd5297af5ee
Dean Earl Bartolabac <dean@link7.co>
2014-02-11 20:24:41 +0800

    updated double single qoutes

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit('No direct script access allowed'); // Exit if accessed directly ?>
 <div class="wrap">
-    <?php echo "<h2>" . __( 'Virtual Pages and Templates Settings' ) . "</h2>";?>  
+    <?php echo '<h2>' . __( 'Virtual Pages and Templates Settings' ) . '</h2>';?>  
     <?php 
     $options = get_option('vpt_options');
     $virtualpageurl = '/shop/%postname%';
@@ -53,7 +53,7 @@
         <table class="form-table">
             <tbody>
             <tr valign="top">
-                <th scope="row"><label for="virtualpageurl"><?php _e("Virtual Page URL: " ); ?></label></th>
+                <th scope="row"><label for="virtualpageurl"><?php _e('Virtual Page URL: '); ?></label></th>
                 <td>
                     <?php if ($use_custom_permalink_structure) $checked = 'checked="checked"'; else $checked = '';?>
                     <label for="users_can_register"><input type="checkbox" value="1" id="use_custom_permalink_structure" <?php echo $checked;?> name="use_custom_permalink_structure">Use custom permalink structure</label>
@@ -68,9 +68,9 @@
             </tr>
 
             <tr>
-            <th scope="row"><?php _e("Post or Post? " ); ?></th>
+            <th scope="row"><?php _e('Post or Post? ' ); ?></th>
             <td>
-                <fieldset><legend class="screen-reader-text"><span><?php _e("Post or Post? " ); ?></span></legend>
+                <fieldset><legend class="screen-reader-text"><span><?php _e('Post or Post? ' ); ?></span></legend>
                 <?php if ($post_type == 'page') $checked = 'checked="checked"'; else $checked = '';?>
                 <label title="Page"><input type="radio" <?php echo $checked;?> value="page" name="post_type"> <span>Page</span></label><br>
                 <?php if ($post_type == 'post') $checked = 'checked="checked"'; else $checked = '';?>
@@ -80,7 +80,7 @@
             </td>
            
             <tr valign="top">
-            <th scope="row"><label for="default_role"><?php _e("Page Template: " ); ?></label></th>
+            <th scope="row"><label for="default_role"><?php _e('Page Template: ' ); ?></label></th>
                 <td>
                 <select id="page_template" name="page_template" style="width: 25em">
                     <?php if (!empty($posts->posts)) :?>
@@ -105,7 +105,7 @@
                 </td>
             </tr>
 
-            <th scope="row"><label for="spinmethod"><?php _e("Spin Method: " ); ?></label></th>
+            <th scope="row"><label for="spinmethod"><?php _e('Spin Method: ' ); ?></label></th>
                 <td>
                 <select id="spinmethod" name="spinmethod"  style="width: 25em">
                     <?php foreach ($spinmethods as $spinid => $spinvalue) :?>
@@ -117,7 +117,7 @@
                 </td>
             </tr>
 
-            <th scope="row"><?php _e("Affect search result " ); ?></th>
+            <th scope="row"><?php _e('Affect search result ' ); ?></th>
             <td>
                 <?php if ($affect_search) $checked = 'checked="checked"'; else $checked = '';?>
                <label for="affect_search"><input type="checkbox" value="1" id="affect_search" <?php echo $checked;?> name="affect_search"></label>
